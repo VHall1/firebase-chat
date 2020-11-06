9c3290748645c3c05afbbd9e4275178004ccbc9f
VHall1 <victorhallpsn@hotmail.com>
2020-11-05 22:53:53 -0300

    fix: removed anoying toast from every new message

--- src/pages/Default.tsx
+++ src/pages/Default.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Box, useToast } from "@chakra-ui/core";
+import { Flex, Text, Box } from "@chakra-ui/core";
 import React, { useContext, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { FirebaseContext } from "../App";
@@ -16,7 +16,6 @@ export interface MessageProps {
 }
 
 const Default: React.FC = () => {
-  const toast = useToast();
   const { auth, firestore } = useContext(FirebaseContext);
 
   const [user] = useAuthState(auth);
@@ -28,14 +27,6 @@ const Default: React.FC = () => {
     const chatBox = document.querySelector(".messages-wrapper");
 
     if (chatBox) {
-      toast({
-        position: "top",
-        render: () => (
-          <Box m={3} color="white" p={3} bg="blue.500" borderRadius="56px">
-            New Message
-          </Box>
-        ),
-      });
       chatBox.scrollTo(0, chatBox.scrollHeight);
     }
   }, [messages]);
@@ -49,8 +40,8 @@ const Default: React.FC = () => {
         style={{ overflowY: "scroll" }}
         direction="column"
         flex="1"
-        py={2}
-        px={4}
+        py={4}
+        px={2}
       >
         {user ? (
           messages &&
